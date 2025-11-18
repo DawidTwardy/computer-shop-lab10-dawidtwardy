06bd325e77456c9c2859a7f0c7a4ffbda986f1e6
DawidTwardy <150292727+DawidTwardy@users.noreply.github.com>
2025-11-18 14:14:56 +0100

    Task 9.1 - add not-found
    Task 9.1 - add not-found

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+// app/not-found.tsx
+
+export default function NotFound() {
+    return (
+        <main className="global-not-found">
+            <h1>Nie znaleziono strony</h1>
+            <p>Strona, której szukasz, nie istnieje.</p>
+            <p>Sprawdź adres URL lub wróć na stronę główną.</p>
+        </main>
+    );
+}
