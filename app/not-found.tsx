2a1731755f9964731ebc581ea6d1876e3a482669
DawidTwardy <150292727+DawidTwardy@users.noreply.github.com>
2025-11-25 15:18:14 +0200

    Add files via upload

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,12 @@
-// app/not-found.tsx
 import Link from 'next/link';
 
 export default function NotFound() {
     return (
-        <main className="global-not-found">
-            <h1>Nie znaleziono strony</h1>
-            <p>Strona, której szukasz, nie istnieje.</p>
+        <main className="text-center p-12 mt-12 border-2 border-[#bcbcb7] rounded-lg">
+            <h1 className="text-3xl text-[#e5e5e1] mb-4">Nie znaleziono strony</h1>
+            <p className="mb-4">Strona, której szukasz, nie istnieje.</p>
             <p>
-                Sprawdź adres URL lub <Link href="/">wróć na stronę główną</Link>.
+                Sprawdź adres URL lub <Link href="/" className="text-[#e5e5e1] underline font-bold hover:text-[#bcbcb7] transition-colors">wróć na stronę główną</Link>.
             </p>
         </main>
     );
---
+++ components/nav-link.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from 'react';
+
+export default function NavLink({ href, children }: { href: string; children: React.ReactNode }) {
+    const path = usePathname();
+    
+    return (
+        <Link
+            href={href}
+            className={path.startsWith(href) ? "active" : undefined}
+        >
+            {children}
+        </Link>
+    );
+}
