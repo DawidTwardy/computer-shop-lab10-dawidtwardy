a6c16be6a8af093acbef6ebc136c24521a9c1428
DawidTwardy <dawidtwardy21@gmail.com>
2025-12-02 01:04:29 +0100

    Task 7.1 handle not-found segments

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,12 @@
-// app/not-found.tsx
 import Link from 'next/link';
 
 export default function NotFound() {
     return (
-        <main className="global-not-found">
-            <h1>Nie znaleziono strony</h1>
-            <p>Strona, której szukasz, nie istnieje.</p>
+        <main className="text-center p-12 mt-12 border-2 border-[#bcbcb7] rounded-lg">
+            <h1 className="text-4xl text-[#e5e5e1] mb-4">Nie znaleziono zasobu (404)</h1>
+            <p className="mb-4">Strona lub produkt, którego szukasz, nie istnieje.</p>
             <p>
-                Sprawdź adres URL lub <Link href="/">wróć na stronę główną</Link>.
+                Sprawdź adres URL lub <Link href="/" className="text-[#e5e5e1] underline font-bold transition-colors duration-200 hover:text-[#bcbcb7]">wróć na stronę główną</Link>.
             </p>
         </main>
     );
